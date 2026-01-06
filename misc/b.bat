b2fcd88bda849ad746234594fe95aba95ec18ce1
HatecrewJP <138243173+HatecrewJP@users.noreply.github.com>
2026-01-07 00:26:34 +0100

    removed assembly generation flag

--- misc/b.bat
+++ misc/b.bat
@@ -4,7 +4,7 @@ set SD=V:\code\
 REM BD: BuildDirectory
 set BD="V:\build\\"
 set DisabledWarnings= -wd4100 -wd4189 -wd4706 -wd4530 -wd4505 -D_CRT_SECURE_NO_WARNINGS
-set CompilerParams= %DisabledWarnings%  -nologo -Od -FC -MT -W4 -WX -Zi -FA 
+set CompilerParams= %DisabledWarnings%  -nologo -Od -FC -MT -W4 -WX -Zi 
 set Libraries=User32.lib D3D11.lib D3DCompiler.lib
 set LinkParams=%Libraries%
 pushd V:\
