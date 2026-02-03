e41e82f22bb3c762215febbbba1a1324a288d2da
HatecrewJP <138243173+HatecrewJP@users.noreply.github.com>
2026-02-03 15:31:11 +0100

    changed file extension and build script

--- misc/b.bat
+++ misc/b.bat
@@ -3,14 +3,14 @@ REM SD:SourceDirectory
 set SD=V:\code\
 REM BD: BuildDirectory
 set BD="V:\build\\"
-set DisabledWarnings= -wd4100 -wd4189 -wd4706 -wd4530 -wd4505 -D_CRT_SECURE_NO_WARNINGS
+set DisabledWarnings= -wd4100 -wd4189 -wd4706 -wd4530 -wd4505 -wd4201 -D_CRT_SECURE_NO_WARNINGS
 set CompilerParams= %DisabledWarnings%  -nologo -Od -FC -MT -W4 -WX -Zi 
-set Libraries=User32.lib D3D11.lib D3DCompiler.lib
+set Libraries=User32.lib D3D11.lib D3DCompiler.lib dxguid.lib
 set LinkParams=%Libraries%
 pushd V:\
 if not exist build mkdir build
 pushd %BD%
 cd
-cl %CompilerParams% %SD%Win32Platform.cpp %LinkParams% /Fe"Direct3D.exe"
+cl %CompilerParams% -Tc %SD%Win32Platform.cpp %LinkParams% /Fe"Direct3D.exe"
 popd
 popd
